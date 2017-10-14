fa63c6ae27972502fcbaf6f6aae97c1f97645cdf
ornichola <1502708+ornichola@users.noreply.github.com>
2017-10-14 23:18:58 +0300

    TechColl Lesson 02

--- [techcoll]/backend/fight.py
+++ [techcoll]/backend/fight.py
@@ -1,24 +1,26 @@
+import random
+
 def hp_count(race_name):
     if race_name == 'human':
         return 200
     elif race_name == 'orc':
         return 400
     elif race_name == 'elf':
-        return = 150
+        return 150
     elif race_name == 'undead':
-        return = 175
+        return 175
     else:
-        return = 200
+        return 200
 
 def hit_power_count(class_name):
-    if class_name == 'warrior:
+    if class_name == 'warrior':
         return 30
     elif class_name == 'mage':
         return 35
     elif class_name == 'rogue':
-        return = 25
+        return 25
     else:
-        return = 30
+        return 30
 
 class Race():
 
@@ -37,3 +39,31 @@ class Fighter():
     def __init__(self, race, fclass):
         self.race = Race(race)
         self.fclass = PlayerClass(fclass)
+
+orc = Fighter("orc", "warrior")
+elf = Fighter("elf", "rogue")
+
+print(orc)
+print(orc.race)
+print(orc.fclass)
+print(orc.race.hp)
+print(orc.fclass.hit_power)
+
+hp_first_fighter = orc.race.hp
+hp_second_fighter = elf.race.hp
+
+def isAlive(hp_first_fighter, hp_second_fighter):
+    if hp_first_fighter > 0 and hp_second_fighter > 0:
+        return True
+    else:
+        return False
+
+while isAlive(hp_first_fighter, hp_second_fighter):
+    print("\nThe {} is attacking!".format(orc.race.race_name))
+    damage = orc.fclass.hit_power * random.random()
+    hp_second_fighter -= round(damage,2)
+    print("{} gets {} damage and now have {}".format(elf.race.race_name, round(damage,2), round(hp_second_fighter,2)))
+    print("\nThe {} is attacking!".format(elf.race.race_name))
+    damage = elf.fclass.hit_power * random.random()
+    hp_first_fighter -= round(damage,2)
+    print("{} gets {} damage and now have {}".format(orc.race.race_name, round(damage,2), round(hp_first_fighter,2)))
